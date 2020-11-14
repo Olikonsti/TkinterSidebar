d4d31ed2cae780f6055bb37e0dc1decea31cdd00
Olikonsti <68354546+Olikonsti@users.noreply.github.com>
2020-11-14 14:30:14 +0100

    Update test.py

--- Code Example(Folder imported)/test.py
+++ Code Example(Folder imported)/test.py
@@ -11,14 +11,14 @@ main_frame.place(x=200, y=0)
 
 
 
-b = SideBar(root)
-b.add_spacer("Allgemeines")
-b.add_button("Home", lambda: HomePage(main_frame), icon="home.png")
-b.add_button("Einstellungen", lambda: SettingsPage(main_frame))
-b.add_spacer("Anderes")
-b.add_button("Test Page", lambda: TestPage(main_frame))
-b.add_button("KundV Website", lambda: print("KundVWebsite"), icon="home.png", tab=False)
-b.finish()
+sidebar = SideBar(root)
+sidebar.add_spacer("Test_")
+sidebar.add_button("Home", lambda: HomePage(main_frame), icon="home.png")
+sidebar.add_button("Settings", lambda: SettingsPage(main_frame))
+sidebar.add_spacer("Other")
+sidebar.add_button("Test Page", lambda: TestPage(main_frame))
+sidebar.add_button("Test", lambda: print("KundVWebsite"), icon="home.png", tab=False)
+sidebar.finish()
 
 
 root.mainloop()
